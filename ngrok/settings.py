fedec698ccb3b25559a8c1b888688543a6952389
ben <smoilbig@126.com>
2018-04-04 14:11:11 +0800

    download page

--- ngrok/settings.py
+++ ngrok/settings.py
@@ -128,4 +128,5 @@ STATICFILES_DIRS = [
     ('css', os.path.join(STATIC_ROOT, 'css').replace('\\', '/')),
     ('js', os.path.join(STATIC_ROOT, 'js').replace('\\', '/')),
     ('images', os.path.join(STATIC_ROOT, 'images').replace('\\', '/')),
+    ('fonts', os.path.join(STATIC_ROOT, 'fonts').replace('\\', '/')),
  ]
